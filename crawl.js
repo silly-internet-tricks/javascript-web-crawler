e7f0fe65bf05ebd7100e41b7ef3a7648873fdb1d
Josh Parkker <jparker@sherpas.ai>
2024-10-05 16:49:31 -0600

    add prettier formatting

--- crawl.js
+++ crawl.js
@@ -1,48 +1,46 @@
-const { JSDOM } = require('jsdom');
-
+const { JSDOM } = require("jsdom");
 
 const getUrlsFromHtml = function getUrlsFromHtml(htmlBody, baseUrl) {
-    const dom = new JSDOM(htmlBody);
-    const nodes = dom.window.document.querySelectorAll('a');
-    const hrefs = [...nodes].map((node) => node.href);
-    const absoluteUrls = hrefs.map((href) => {
-        if (!href.match(/^(https?:)?\/\//i)) {
-            if (href.match(/^about:blank#/i)) return baseUrl;
-            if (href.match(/^\/[^\/]/)) {
-                const domainMatch = baseUrl.match(/^(.*?)\/[^\/]/);
-                const domainPart = domainMatch ? domainMatch[1] : baseUrl;
-                return `${domainPart}${href}`;
-            }
+  const dom = new JSDOM(htmlBody);
+  const nodes = dom.window.document.querySelectorAll("a");
+  const hrefs = [...nodes].map((node) => node.href);
+  const absoluteUrls = hrefs.map((href) => {
+    if (!href.match(/^(https?:)?\/\//i)) {
+      if (href.match(/^about:blank#/i)) return baseUrl;
+      if (href.match(/^\/[^\/]/)) {
+        const domainMatch = baseUrl.match(/^(.*?)\/[^\/]/);
+        const domainPart = domainMatch ? domainMatch[1] : baseUrl;
+        return `${domainPart}${href}`;
+      }
 
-            if (href.match(/^\.\.\//)) {
-            let baseUrlPartial = baseUrl;
-            // for now I am only considering the case where .. is followed directly by slash
-            // I am not sure yet what to do if .. is followed by anything else
-            while (href.match(/^\.\.\//)) {
-                href = href.replace(/^\.\.\//, '');
-                baseUrlPartial = baseUrlPartial.replace(/\/[^\/]*$/, '');
-            }
-
-            return `${baseUrlPartial}/${href}`;
-}
-
-            return `${baseUrl}/${href}`;
+      if (href.match(/^\.\.\//)) {
+        let baseUrlPartial = baseUrl;
+        // for now I am only considering the case where .. is followed directly by slash
+        // I am not sure yet what to do if .. is followed by anything else
+        while (href.match(/^\.\.\//)) {
+          href = href.replace(/^\.\.\//, "");
+          baseUrlPartial = baseUrlPartial.replace(/\/[^\/]*$/, "");
         }
-        return href;
-    });
-    return absoluteUrls;
+
+        return `${baseUrlPartial}/${href}`;
+      }
+
+      return `${baseUrl}/${href}`;
+    }
+    return href;
+  });
+  return absoluteUrls;
 };
 
 const normalizeUrl = function normalizeUrl(inputUrl) {
-    if (!(typeof inputUrl === 'string')) return '';
-    return decodeURIComponent(inputUrl)
-        .trim()
-        .replace(/^https?:\/\//i, '')
-        .replace(/#.*/, '')
-        .replace(/\?.*/, '')
-        .replace(/\/+$/, '')
-        .toLocaleLowerCase();
+  if (!(typeof inputUrl === "string")) return "";
+  return decodeURIComponent(inputUrl)
+    .trim()
+    .replace(/^https?:\/\//i, "")
+    .replace(/#.*/, "")
+    .replace(/\?.*/, "")
+    .replace(/\/+$/, "")
+    .toLocaleLowerCase();
 };
 
 module.exports = { normalizeUrl, getUrlsFromHtml };
-
--- crawl.test.js
+++ crawl.test.js
@@ -1,96 +1,78 @@
-const { test, expect } = require('@jest/globals');
+const { test, expect } = require("@jest/globals");
 
-const { normalizeUrl, getUrlsFromHtml } = require('./crawl.js');
+const { normalizeUrl, getUrlsFromHtml } = require("./crawl.js");
 
-const sum = (a,b) => a + b;
+const sum = (a, b) => a + b;
 
-test('adds 1 + 2 to equal 3', () => {
+test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('normalizes urls', () => {
-    const urls = [
-        'https://blog.boot.dev/path/',
-        'https://blog.boot.dev/path',
-        'http://blog.boot.dev/path/',
-        'http://blog.boot.dev/path',
-    ];
+test("normalizes urls", () => {
+  const urls = [
+    "https://blog.boot.dev/path/",
+    "https://blog.boot.dev/path",
+    "http://blog.boot.dev/path/",
+    "http://blog.boot.dev/path",
+  ];
 
-    urls.forEach((url) => {
-        expect(normalizeUrl(url)).toBe('blog.boot.dev/path');
-    });
+  urls.forEach((url) => {
+    expect(normalizeUrl(url)).toBe("blog.boot.dev/path");
+  });
 });
 
-test('normalizes urls with uppercase', () => {
-    const urls = [
-        'BLOG.BOOT.DEV',
-        'Blog.Boot.Dev/',
-        'bLoG.bOoT.dev',
-    ];
+test("normalizes urls with uppercase", () => {
+  const urls = ["BLOG.BOOT.DEV", "Blog.Boot.Dev/", "bLoG.bOoT.dev"];
 
-    urls.forEach((url) => {
-        expect(normalizeUrl(url)).toBe('blog.boot.dev');
-    });
+  urls.forEach((url) => {
+    expect(normalizeUrl(url)).toBe("blog.boot.dev");
+  });
 });
 
-test('normalizes urls with fragments and query strings', () => {
-    const urls = [
-        'boot.dev/path/to/resource#heading',
-        'boot.dev/path/to/resource#heading-with-dashes',
-        'boot.dev/path/to/resource#heading?query=hello',
-        'boot.dev/path/to/resource?query=before-fragment#heading',
-        'boot.dev/path/to/resource?heading=idk',
-        'boot.dev/path/to/reSource?heading=first,footer=second,body=third',
-        'boot.dev/path/to/reSource?heading=first&footer=second&body=third',
-        'boot.dev/path/to/resource?heading=first,footer=second,body=third#hash-fragment',
-        'boot.dev/path/to/resource?heading=first&footer=second&body=third#hash-fragment',
-    ];
+test("normalizes urls with fragments and query strings", () => {
+  const urls = [
+    "boot.dev/path/to/resource#heading",
+    "boot.dev/path/to/resource#heading-with-dashes",
+    "boot.dev/path/to/resource#heading?query=hello",
+    "boot.dev/path/to/resource?query=before-fragment#heading",
+    "boot.dev/path/to/resource?heading=idk",
+    "boot.dev/path/to/reSource?heading=first,footer=second,body=third",
+    "boot.dev/path/to/reSource?heading=first&footer=second&body=third",
+    "boot.dev/path/to/resource?heading=first,footer=second,body=third#hash-fragment",
+    "boot.dev/path/to/resource?heading=first&footer=second&body=third#hash-fragment",
+  ];
 
-    urls.forEach((url) => {
-        expect(normalizeUrl(url)).toBe('boot.dev/path/to/resource');
-    });
+  urls.forEach((url) => {
+    expect(normalizeUrl(url)).toBe("boot.dev/path/to/resource");
+  });
 });
 
-test('normalizes urls with space padding', () => {
-    const urls = [
-        'boot.dev        ',
-        '  boot.dev/',
-        '     https://boot.dev    ',
-    ];
+test("normalizes urls with space padding", () => {
+  const urls = ["boot.dev        ", "  boot.dev/", "     https://boot.dev    "];
 
-    urls.forEach((url) => {
-        expect(normalizeUrl(url)).toBe('boot.dev');
-    });
+  urls.forEach((url) => {
+    expect(normalizeUrl(url)).toBe("boot.dev");
+  });
 });
 
-test('normalizes urls which have uriencoded chars', () => {
-    const urls = [
-        'https://blog.boo%20oot.dev',
-        // for now I am going to say that this url should be decoded first, and then the https:// should be discarded
-        'https%3A%2F%2Fblog.boo%20oot.dev',
-    ];
+test("normalizes urls which have uriencoded chars", () => {
+  const urls = [
+    "https://blog.boo%20oot.dev",
+    // for now I am going to say that this url should be decoded first, and then the https:// should be discarded
+    "https%3A%2F%2Fblog.boo%20oot.dev",
+  ];
 
-    urls.forEach((url) => {
-        expect(normalizeUrl(url)).toBe('blog.boo oot.dev');
-    });
+  urls.forEach((url) => {
+    expect(normalizeUrl(url)).toBe("blog.boo oot.dev");
+  });
 });
 
-test('normalize url does not choke on obvious bad inputs', () => {
-    const urls = [
-        {},
-        [],
-        '',
-        undefined,
-        null,
-        true,
-        false,
-        0,
-        1,
-    ];
+test("normalize url does not choke on obvious bad inputs", () => {
+  const urls = [{}, [], "", undefined, null, true, false, 0, 1];
 
-    urls.forEach((url) => {
-        expect(normalizeUrl(url)).toBe('');
-    });
+  urls.forEach((url) => {
+    expect(normalizeUrl(url)).toBe("");
+  });
 });
 
 const htmlTestBody = `
@@ -108,23 +90,25 @@ const htmlTestBody = `
 </html>
 `;
 
-test('relative URLs are converted to absolute URLs', () => {
-    const results = getUrlsFromHtml(htmlTestBody, 'blog.boot.dev/path/to/current-page')
-    // NOTE: I did not include the protocol on the front of the base url.
-    // I don't think it's necessary but I'm not positive
+test("relative URLs are converted to absolute URLs", () => {
+  const results = getUrlsFromHtml(
+    htmlTestBody,
+    "blog.boot.dev/path/to/current-page",
+  );
+  // NOTE: I did not include the protocol on the front of the base url.
+  // I don't think it's necessary but I'm not positive
 
-    const expected = [
-        // JSDOM put a trailing slash at the end of the domain. Not sure why, but I am putting it here as well for now.
-        'https://blog.boot.dev/',
-        'blog.boot.dev/relative/url',
-        '//google.com',
-        'blog.boot.dev/path/to/current-page/some/other/resource',
-        'blog.boot.dev/path/to/sibling/resource',
-        'blog.boot.dev/path/to/current-page',
-        'blog.boot.dev/path/cousin/resource',
-    ];
+  const expected = [
+    // JSDOM put a trailing slash at the end of the domain. Not sure why, but I am putting it here as well for now.
+    "https://blog.boot.dev/",
+    "blog.boot.dev/relative/url",
+    "//google.com",
+    "blog.boot.dev/path/to/current-page/some/other/resource",
+    "blog.boot.dev/path/to/sibling/resource",
+    "blog.boot.dev/path/to/current-page",
+    "blog.boot.dev/path/cousin/resource",
+  ];
 
-    expect(results.length).toBe(expected.length);
-    expect(results).toEqual(expected);
+  expect(results.length).toBe(expected.length);
+  expect(results).toEqual(expected);
 });
-
