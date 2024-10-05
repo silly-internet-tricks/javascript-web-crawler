9a993bb9f4724720074720ca9226992746061db4
Josh Parkker <jparker@sherpas.ai>
2024-10-05 16:46:49 -0600

    create the get urls from html function

--- crawl.js
+++ crawl.js
@@ -1,3 +1,38 @@
+const { JSDOM } = require('jsdom');
+
+
+const getUrlsFromHtml = function getUrlsFromHtml(htmlBody, baseUrl) {
+    const dom = new JSDOM(htmlBody);
+    const nodes = dom.window.document.querySelectorAll('a');
+    const hrefs = [...nodes].map((node) => node.href);
+    const absoluteUrls = hrefs.map((href) => {
+        if (!href.match(/^(https?:)?\/\//i)) {
+            if (href.match(/^about:blank#/i)) return baseUrl;
+            if (href.match(/^\/[^\/]/)) {
+                const domainMatch = baseUrl.match(/^(.*?)\/[^\/]/);
+                const domainPart = domainMatch ? domainMatch[1] : baseUrl;
+                return `${domainPart}${href}`;
+            }
+
+            if (href.match(/^\.\.\//)) {
+            let baseUrlPartial = baseUrl;
+            // for now I am only considering the case where .. is followed directly by slash
+            // I am not sure yet what to do if .. is followed by anything else
+            while (href.match(/^\.\.\//)) {
+                href = href.replace(/^\.\.\//, '');
+                baseUrlPartial = baseUrlPartial.replace(/\/[^\/]*$/, '');
+            }
+
+            return `${baseUrlPartial}/${href}`;
+}
+
+            return `${baseUrl}/${href}`;
+        }
+        return href;
+    });
+    return absoluteUrls;
+};
+
 const normalizeUrl = function normalizeUrl(inputUrl) {
     if (!(typeof inputUrl === 'string')) return '';
     return decodeURIComponent(inputUrl)
@@ -9,5 +44,5 @@ const normalizeUrl = function normalizeUrl(inputUrl) {
         .toLocaleLowerCase();
 };
 
-module.exports = { normalizeUrl };
+module.exports = { normalizeUrl, getUrlsFromHtml };
 
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@jest/globals');
 
-const { normalizeUrl } = require('./crawl.js');
+const { normalizeUrl, getUrlsFromHtml } = require('./crawl.js');
 
 const sum = (a,b) => a + b;
 
@@ -87,8 +87,44 @@ test('normalize url does not choke on obvious bad inputs', () => {
         0,
         1,
     ];
+
     urls.forEach((url) => {
         expect(normalizeUrl(url)).toBe('');
     });
+});
+
+const htmlTestBody = `
+  <html>
+    <body>
+        <p id="anchor-only">top of body</p>
+        <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        <a href="/relative/url">hi</a>
+        <a href="//google.com">scheme relative url</a>
+        <a href="some/other/resource">sub resource</a>
+        <a href="../sibling/resource">back up the directory tree</a>
+        <a href="#anchor-only">anchor only</a>
+        <a href="../../cousin/resource">further back up the directory tree</a>
+    </body>
+</html>
+`;
+
+test('relative URLs are converted to absolute URLs', () => {
+    const results = getUrlsFromHtml(htmlTestBody, 'blog.boot.dev/path/to/current-page')
+    // NOTE: I did not include the protocol on the front of the base url.
+    // I don't think it's necessary but I'm not positive
+
+    const expected = [
+        // JSDOM put a trailing slash at the end of the domain. Not sure why, but I am putting it here as well for now.
+        'https://blog.boot.dev/',
+        'blog.boot.dev/relative/url',
+        '//google.com',
+        'blog.boot.dev/path/to/current-page/some/other/resource',
+        'blog.boot.dev/path/to/sibling/resource',
+        'blog.boot.dev/path/to/current-page',
+        'blog.boot.dev/path/cousin/resource',
+    ];
+
+    expect(results.length).toBe(expected.length);
+    expect(results).toEqual(expected);
 });
 
